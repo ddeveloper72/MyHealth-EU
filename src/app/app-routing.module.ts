d495b0e0bd96dd5f8338942e5eba584d3ff0ab80
Duncan Falconer <duncan.niamh@gmail.com>
2024-04-30 14:18:00 +0100

    repurposed & renamed unused components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,20 @@ import { NgModule, ViewChildren } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Routes } from '@angular/router';
 
-import { EtehrComponent } from './etehr/etehr.component';
-import { HpinnoticeComponent } from './hpinnotice/hpinnotice.component';
+import { EtEHRComponent } from './etehr/etehr.component';
+import { XtEHRComponent } from './xtehr/xtehr.component';
 import { PartnershipsComponent } from './partnerships/partnerships.component';
 import { PathedComponent } from './pathed/pathed.component';
-import { PinnoticeComponent } from './pinnotice/pinnotice.component';
+import { PotentialComponent } from './potential/potential.component';
 import { XshareComponent } from './xshare/xshare.component';
 import { MyhealthComponent } from './myhealth/myhealth.component';
 import { MySiteComponent } from './my-site/my-site.component';
 
 export const routes: Routes = [
   { path: 'myhealth', component: MyhealthComponent },
-  { path: 'pinnotice', component: PinnoticeComponent },
-  { path: 'hpinnotice', component: HpinnoticeComponent },
-  { path: 'etehr', component: EtehrComponent },
+  { path: 'potential', component: PotentialComponent },
+  { path: 'xtehr', component: XtEHRComponent },
+  { path: 'etehr', component: EtEHRComponent },
   { path: 'partherships', component: PartnershipsComponent },
   { path: 'pathed', component: PathedComponent },
   { path: 'xshare', component: XshareComponent },
