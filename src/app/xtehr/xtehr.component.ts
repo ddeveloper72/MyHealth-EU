ed25a09c7f271cc6fcdc09578b8f24af3281536b
Duncan Falconer <duncan.niamh@gmail.com>
2024-04-30 14:23:56 +0100

    repurposed & renamed ts file

---
+++ src/app/xtehr/xtehr.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xtehr',
+  standalone: false,
+  templateUrl: './xtehr.component.html',
+  styleUrl: './xtehr.component.css'
+})
+export class XtEHRComponent {
+
+}
