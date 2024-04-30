319ab73d40656ec72bd35730cb5318e768adbebe
Duncan Falconer <duncan.niamh@gmail.com>
2024-04-30 21:35:42 +0100

    added imports & paths for new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,9 @@ import { PotentialComponent } from './potential/potential.component';
 import { XshareComponent } from './xshare/xshare.component';
 import { MyhealthComponent } from './myhealth/myhealth.component';
 import { MySiteComponent } from './my-site/my-site.component';
+import { UNICOMComponent } from './unicom/unicom.component';
+import { IPSComponent } from './ips/ips.component';
+import { IHEComponent } from './ihe/ihe.component';
 
 export const routes: Routes = [
   { path: 'myhealth', component: MyhealthComponent },
@@ -19,6 +22,9 @@ export const routes: Routes = [
   { path: 'partherships', component: PartnershipsComponent },
   { path: 'pathed', component: PathedComponent },
   { path: 'xshare', component: XshareComponent },
+  { path: 'ihe', component: IHEComponent },
+  { path: 'ips', component: IPSComponent },
+  { path: 'unicom', component: UNICOMComponent },
   { path: 'welcome', component: MySiteComponent },
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
 ];
