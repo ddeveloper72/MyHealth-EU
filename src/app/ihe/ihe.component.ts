7691f1e132b0be7e67627eaafc656f1e3403065e
Duncan Falconer <duncan.niamh@gmail.com>
2024-04-30 21:33:39 +0100

    added IHE component

---
+++ src/app/ihe/ihe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ihe',
+  standalone: true,
+  imports: [],
+  templateUrl: './ihe.component.html',
+  styleUrl: './ihe.component.css'
+})
+export class IHEComponent {
+
+}
