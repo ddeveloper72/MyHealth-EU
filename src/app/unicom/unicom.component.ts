0257d1ad6583672827202dfffe466241aad294f3
Duncan Falconer <duncan.niamh@gmail.com>
2024-04-30 21:34:45 +0100

    added UNICOM pomponent

---
+++ src/app/unicom/unicom.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unicom',
+  standalone: true,
+  imports: [],
+  templateUrl: './unicom.component.html',
+  styleUrl: './unicom.component.css'
+})
+export class UNICOMComponent {
+
+}
