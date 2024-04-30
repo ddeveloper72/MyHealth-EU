46c1469cd580f81700c2ac43904ce24e94c8dca4
Duncan Falconer <duncan.niamh@gmail.com>
2024-04-30 14:23:49 +0100

    repurposed & renamed ts file

---
+++ src/app/potential/potential.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-potential',
+  standalone: false,
+  templateUrl: './potential.component.html',
+  styleUrl: './potential.component.css',
+})
+export class PotentialComponent {}
