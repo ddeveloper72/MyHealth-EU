ea34e8c3b04d197004809f322a3cde77c692f86c
Duncan Falconer <duncan.niamh@gmail.com>
2024-04-30 22:21:52 +0100

    configured component for standalone

--- src/app/ihe/ihe.component.ts
+++ src/app/ihe/ihe.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ihe',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './ihe.component.html',
   styleUrl: './ihe.component.css'
 })
