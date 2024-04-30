22a4b2584c2a7b33cf4d174ae9ebe3caca5f32ae
Duncan Falconer <duncan.niamh@gmail.com>
2024-04-30 21:34:03 +0100

    added IPS component

---
+++ src/app/ips/ips.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ips',
+  standalone: true,
+  imports: [],
+  templateUrl: './ips.component.html',
+  styleUrl: './ips.component.css'
+})
+export class IPSComponent {
+
+}
